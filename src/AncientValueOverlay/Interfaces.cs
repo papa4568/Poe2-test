3f03cec852ee6f237425d05ada06ed88c4941b9e
papa4568 <fpspro0421@gmail.com>
2026-07-02 16:15:24 -0500

    Simplify service interfaces

--- src/AncientValueOverlay/Interfaces.cs
+++ src/AncientValueOverlay/Interfaces.cs
@@ -1,23 +1,7 @@
 namespace AncientValueOverlay;
 
-public interface IPriceSource
+public interface IAppStep
 {
-    Task<PriceFetchResult> FetchAsync(AppConfig config, CancellationToken ct = default);
-}
-
-public interface IRewardReader
-{
-    Task<IReadOnlyList<RewardRead>> ReadAsync(CalibrationProfile profile, CancellationToken ct = default);
-}
-
-public interface IValuePresenter
-{
-    void Show(PanelAdvice advice, PriceSourceHealth health);
-    void Hide();
-}
-
-public interface IConfigStore
-{
-    Task<AppConfig> LoadAsync(CancellationToken ct = default);
-    Task SaveAsync(AppConfig config, CancellationToken ct = default);
+    string Name { get; }
+    string Status { get; }
 }
